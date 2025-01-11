f974c78b043af4885a9d877dad0be59bf723b478
Pedro Carvalho <pedro8.carvalho@gmail.com>
2025-01-11 11:50:09 -0300

    first version of my program

---
+++ src/application/Program.java
@@ -0,0 +1,48 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+import entities.LogRecord;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter file full path: ");
+		String filePath = sc.nextLine();
+		
+		Set<LogRecord> totalUsers = new HashSet<>();
+		try (BufferedReader br = new BufferedReader(new FileReader(filePath))){
+			
+			String newLine = br.readLine();
+			
+			while (newLine != null) {
+				
+				String[] data = newLine.split(" ");
+				
+				totalUsers.add(new LogRecord(data[0], Instant.parse(data[1])));
+				
+				newLine = br.readLine();
+			}
+		}
+		catch (IOException ioe) {
+			System.out.println(ioe.getMessage());
+		}
+		
+		
+		System.out.printf("Total de usuarios: %d", totalUsers.size());
+		// primeiro comentario
+		sc.close();
+		
+		
+	}
+
+}
